903d5f3399e80fa3aab4ac7959e0aef255d172b5
Andrew S. <asdznpro@gmail.com>
2024-09-10 06:24:05 +0700

    refactor: update all

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -3,16 +3,32 @@ import { useLocation } from 'react-router-dom'
 import { useAppSelector, useAppDispatch, useDynamicAlignment } from 'hooks'
 import { setTheme, selectTheme, setLanguage, selectLanguage } from 'store'
 
+import {
+	BREAKPOINTS_TAILWIND,
+	useBreakpoints,
+	useToggle,
+	useClickOutside,
+} from '@siberiacancode/reactuse'
+
 import styles from './Header.module.scss'
 import { LanguageType, ThemeType } from 'types'
 
 import { AppLink, Button, Counter, Spinner } from 'components/ui'
 
 import { CvLogoIcon, CvLogo, CvLogoAbbreviated } from 'components/ui'
-import { Icon28MoonOutline, Icon28SunOutline } from '@vkontakte/icons'
+import {
+	Icon28CancelOutline,
+	Icon28MoonOutline,
+	Icon28Menu,
+	Icon28SunOutline,
+} from '@vkontakte/icons'
+import { useRef } from 'react'
 
 const Header: React.FC = () => {
 	const { screenWidth } = useDynamicAlignment()
+
+	const { greaterOrEqual, smallerOrEqual, current } =
+		useBreakpoints(BREAKPOINTS_TAILWIND)
 
 	const dispatch = useAppDispatch()
 	const location = useLocation()
@@ -44,6 +60,21 @@ const Header: React.FC = () => {
 		localStorage.setItem('theme', newTheme)
 	}
 
+	//
+
+	const [on, toggle] = useToggle()
+
+	const dropdownRef = useRef<HTMLDivElement | null>(null)
+	const buttonRef = useRef<HTMLButtonElement | null>(null)
+
+	useClickOutside(dropdownRef, (event) => {
+		if (buttonRef.current && buttonRef.current.contains(event.target as Node)) {
+			return
+		}
+
+		toggle(false)
+	})
+
 	return (
 		<header className={styles.root}>
 			<div className={styles.container}>
@@ -60,7 +91,45 @@ const Header: React.FC = () => {
 				</div>
 
 				<div className={styles['header-part']}>
-					{screenWidth > 1440 &&
+					{greaterOrEqual('md') && (
+						<>
+							<Button
+								onClick={handleToggleTheme}
+								appearance={theme.mode === 'light' ? 'neutral' : 'accent'}
+								before={
+									theme.mode === 'light' ? (
+										<Icon28MoonOutline width={24} height={24} />
+									) : (
+										<Icon28SunOutline width={24} height={24} />
+									)
+								}
+								rounded
+							/>
+
+							<Button
+								onClick={handleToggleLanguage}
+								mode="outline"
+								appearance="neutral"
+								before={
+									<img
+										src={
+											language.lang === 'ru'
+												? 'https://asdznpro-cv.hb.ru-msk.vkcs.cloud/assets/icons/countries/great-britain_rounded_28.svg'
+												: 'https://asdznpro-cv.hb.ru-msk.vkcs.cloud/assets/icons/countries/russia_rounded_28.svg'
+										}
+										alt="Language"
+										width={screenWidth > 576 ? 24 : 32}
+										height={screenWidth > 576 ? 24 : 32}
+									/>
+								}
+								rounded
+							>
+								{screenWidth > 576 && (language.lang === 'ru' ? 'EN' : 'RU')}
+							</Button>
+						</>
+					)}
+
+					{greaterOrEqual('2xl') &&
 						(storeCvMapData ? (
 							storeCvMapData.data.map((item, index) => (
 								<Button
@@ -120,50 +189,116 @@ const Header: React.FC = () => {
 							</>
 						))}
 
-					{screenWidth <= 1440 && (
+					{smallerOrEqual('2xl') && (
 						<Button
+							ref={buttonRef}
+							onClick={() => toggle()}
 							appearance="neutral"
 							mode="secondary"
-							before={<Spinner />}
+							before={
+								on ? (
+									<Icon28CancelOutline width={24} height={24} />
+								) : (
+									<Icon28Menu width={24} height={24} />
+								)
+							}
 							rounded
 						>
-							{/* #меню */}
+							#меню
 						</Button>
 					)}
 
-					<Button
-						onClick={handleToggleTheme}
-						appearance={theme.mode === 'light' ? 'neutral' : 'accent'}
-						before={
-							theme.mode === 'light' ? (
-								<Icon28MoonOutline width={24} height={24} />
-							) : (
-								<Icon28SunOutline width={24} height={24} />
-							)
-						}
-						rounded
-					/>
+					{storeCvMapData && on && smallerOrEqual('2xl') && (
+						<div ref={dropdownRef} className={styles.dropdown}>
+							<div className={styles.dropdownList}>
+								{smallerOrEqual('md') && (
+									<div
+										className="ui-flex ui-gap-6-px ui-w-full"
+										style={{ flex: '1 1 50%' }}
+									>
+										<Button
+											onClick={handleToggleTheme}
+											buttonSize="md"
+											appearance={theme.mode === 'light' ? 'neutral' : 'accent'}
+											before={
+												theme.mode === 'light' ? (
+													<Icon28MoonOutline width={28} height={28} />
+												) : (
+													<Icon28SunOutline width={28} height={28} />
+												)
+											}
+											style={{ flex: '1 1 50%' }}
+										>
+											{theme.mode === 'light' ? 'Light' : 'Dark'}
+										</Button>
 
-					<Button
-						onClick={handleToggleLanguage}
-						mode="outline"
-						appearance="neutral"
-						before={
-							<img
-								src={
-									language.lang === 'ru'
-										? 'https://asdznpro-cv.hb.ru-msk.vkcs.cloud/assets/icons/countries/great-britain_rounded_28.svg'
-										: 'https://asdznpro-cv.hb.ru-msk.vkcs.cloud/assets/icons/countries/russia_rounded_28.svg'
-								}
-								alt="Language"
-								width={screenWidth > 576 ? 24 : 32}
-								height={screenWidth > 576 ? 24 : 32}
-							/>
-						}
-						rounded
-					>
-						{screenWidth > 576 && (language.lang === 'ru' ? 'EN' : 'RU')}
-					</Button>
+										<Button
+											onClick={handleToggleLanguage}
+											buttonSize="md"
+											mode="outline"
+											appearance="neutral"
+											before={
+												<img
+													src={
+														language.lang === 'ru'
+															? 'https://asdznpro-cv.hb.ru-msk.vkcs.cloud/assets/icons/countries/great-britain_rounded_28.svg'
+															: 'https://asdznpro-cv.hb.ru-msk.vkcs.cloud/assets/icons/countries/russia_rounded_28.svg'
+													}
+													alt="Language"
+													width={28}
+													height={28}
+												/>
+											}
+											style={{ flex: '1 1 50%' }}
+										>
+											{language.lang === 'ru' ? 'EN' : 'RU'}
+										</Button>
+									</div>
+								)}
+
+								{storeCvMapData.data.map((item, index) => (
+									<Button
+										key={index}
+										to={
+											storePortfolioData &&
+											storePortfolioData.data.length < 1 &&
+											item.pathname === 'portfolio'
+												? ''
+												: '/' + item.pathname
+										}
+										buttonSize="md"
+										mode={
+											location.pathname.startsWith('/' + item.pathname)
+												? 'secondary'
+												: 'ghost'
+										}
+										appearance="neutral"
+										after={
+											item.pathname === 'portfolio' &&
+											storePortfolioData && (
+												<Counter counterSize="sm" appearance="neutral">
+													{storePortfolioData.data.length}
+												</Counter>
+											)
+										}
+										disabled={
+											(item.pathname === 'portfolio' &&
+												storePortfolioData &&
+												storePortfolioData.data.length === 0) ||
+											undefined
+										}
+										stretched
+									>
+										{'#' +
+											item.name
+												.toLowerCase()
+												.replace(/\s/g, '_')
+												.replace(/\./g, '')}
+									</Button>
+								))}
+							</div>
+						</div>
+					)}
 				</div>
 			</div>
 		</header>
--- src/components/ui/blocks/button/Button.tsx
+++ src/components/ui/blocks/button/Button.tsx
@@ -1,85 +1,97 @@
+import React, { forwardRef } from 'react'
 import { Link } from 'react-router-dom'
 
 import styles from './Button.module.scss'
 import ButtonProps from './Button.interface'
 
-const Button: React.FC<ButtonProps> = props => {
-	const {
-		children,
-		mode = 'primary',
-		appearance = 'accent',
-		buttonSize = 'sm',
-		rounded,
-		disabled,
-		noneAction,
-		stretched,
-		before,
-		after,
-		className,
-		to,
-		href,
-		target,
-		...restProps
-	} = props
+const Button = forwardRef<HTMLButtonElement | HTMLAnchorElement, ButtonProps>(
+	(props, ref) => {
+		const {
+			children,
+			mode = 'primary',
+			appearance = 'accent',
+			buttonSize = 'sm',
+			rounded,
+			disabled,
+			noneAction,
+			stretched,
+			before,
+			after,
+			className,
+			to,
+			href,
+			target,
+			...restProps
+		} = props
 
-	let Component: React.ElementType = 'button' as React.ElementType
-	let additionalProps = {}
+		let Component: React.ElementType = 'button' as React.ElementType
+		let additionalProps = {}
 
-	if (href) {
-		Component = 'a'
-		additionalProps = { href, target, ...additionalProps }
-	} else if (to) {
-		Component = Link
-		additionalProps = { to, target, ...additionalProps }
-	} else {
-		Component = 'button'
-		additionalProps = {
-			disabled: disabled || noneAction,
-			...additionalProps,
+		if (href) {
+			Component = 'a'
+			additionalProps = { href, target, ...additionalProps }
+		} else if (to) {
+			Component = Link
+			additionalProps = { to, target, ...additionalProps }
+		} else {
+			Component = 'button'
+			additionalProps = {
+				disabled: disabled || noneAction,
+				...additionalProps,
+			}
 		}
-	}
 
-	return (
-		<Component
-			{...restProps}
-			{...additionalProps}
-			className={[
-				styles.root,
-				styles[`mode-${mode}`],
-				styles[`${appearance}`],
-				styles[`size-${buttonSize}`],
-				rounded && styles['rounded'],
-				(to || href) && disabled && styles['disabled-link'],
-				stretched && styles.stretched,
-				!children && (!before || !after) ? styles['button-icon'] : '',
-				noneAction && styles['none-action'],
-				disabled && styles['disabled'],
-				className,
-			]
-				.join(' ')
-				.trim()}
-		>
-			<span className={styles.in}>
-				{before && <span className={styles.before}>{before}</span>}
+		return (
+			<Component
+				{...restProps}
+				{...additionalProps}
+				ref={ref}
+				className={[
+					styles.root,
+					styles[`mode-${mode}`],
+					styles[`${appearance}`],
+					styles[`size-${buttonSize}`],
+					rounded && styles['rounded'],
+					(to || href) && disabled && styles['disabled-link'],
+					stretched && styles.stretched,
+					!children && (!before || !after) ? styles['button-icon'] : '',
+					noneAction && styles['none-action'],
+					disabled && styles['disabled'],
+					className,
+				]
+					.join(' ')
+					.trim()}
+			>
+				<span className={styles.in}>
+					{before && <span className={styles.before}>{before}</span>}
 
-				{children && (
-					<span
-						className={styles.content}
-						style={{
-							paddingLeft:
-								!before && !after ? '0.48em' : !before && after ? '0.24em' : 0,
-							paddingRight:
-								!before && !after ? '0.48em' : before && !after ? '0.24em' : 0,
-						}}
-					>
-						{children}
-					</span>
-				)}
+					{children && (
+						<span
+							className={styles.content}
+							style={{
+								paddingLeft:
+									!before && !after
+										? '0.48em'
+										: !before && after
+											? '0.24em'
+											: 0,
+								paddingRight:
+									!before && !after
+										? '0.48em'
+										: before && !after
+											? '0.24em'
+											: 0,
+							}}
+						>
+							{children}
+						</span>
+					)}
 
-				{after && <span className={styles.after}>{after}</span>}
-			</span>
-		</Component>
-	)
-}
+					{after && <span className={styles.after}>{after}</span>}
+				</span>
+			</Component>
+		)
+	},
+)
 
 export { Button }
